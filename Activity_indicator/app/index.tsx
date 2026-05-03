f4e2e95ce8944b5f4de34d063b7e983ccb0ae908
Tejas Katkar <tkatkar1256@gmail.com>
2026-05-03 20:39:40 +0530

    learned custom activity indicator

--- Activity_indicator/app/index.tsx
+++ Activity_indicator/app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator } from "react-native";
+import lottieView from "lottie-react-native";
+import LottieView from "lottie-react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        paddingTop: 50,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+     <ActivityIndicator size="large"/> 
+
+     <LottieView
+      source={require("./../assets/loading.json")}
+      autoPlay
+      loop
+      style={{ width: 50, height: 500 }}
+     />
     </View>
   );
 }
